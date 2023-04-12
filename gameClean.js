e2c439ad9ef651502afd4189b885c65bd87cae05
itan <itanfelz@uc.cl>
2023-04-12 12:28:48 -0400

    fantasmas

--- gameClean.js
+++ gameClean.js
@@ -1,15 +1,15 @@
-const canvas = document.getElementById('canvas');
-const canvasContext = canvas.getContext('2d');
-canvasContext.font = '16px arial';
-canvasContext.fillStyle = '#ffff';
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+canvasContext.font = "16px arial";
+canvasContext.fillStyle = "#ffff";
 canvasContext.fillText(
-  'CLICK HERE TO START',
+  "CLICK HERE TO START",
   canvas.width / 2,
   canvas.height / 2
 );
-const pacmanFrames = document.getElementById('animation');
-const pacmanSecondFrames = document.getElementById('animation_green');
-const ghostFrames = document.getElementById('ghosts');
+const pacmanFrames = document.getElementById("animation");
+const pacmanSecondFrames = document.getElementById("animation_green");
+const ghostFrames = document.getElementById("ghosts");
 
 const createRect = (x, y, width, height, color) => {
   canvasContext.fillStyle = color;
@@ -22,6 +22,14 @@ const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_BOTTOM = 1;
 
+let ghostCount = 4;
+let ghostImageLocations = [
+  { x: 0, y: 0 },
+  { x: 176, y: 0 },
+  { x: 0, y: 121 },
+  { x: 176, y: 121 },
+];
+
 // Game variables
 const fps = 30;
 const oneBlockSize = 20; // defines the size of the map
@@ -29,8 +37,42 @@ const oneBlockSize = 20; // defines the size of the map
 // wall visuals
 const wallSpaceWidth = oneBlockSize / 1.6;
 const wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
-const wallInnerColor = 'black';
+const wallInnerColor = "black";
 
+let randomTargetsForGhosts = [
+  { x: 1 * oneBlockSize, y: 1 * oneBlockSize },
+  { x: 1 * oneBlockSize, y: (map.length - 2) * oneBlockSize },
+  { x: (map[0].length - 2) * oneBlockSize, y: oneBlockSize },
+  {
+    x: (map[0].length - 2) * oneBlockSize,
+    y: (map.length - 2) * oneBlockSize,
+  },
+];
+
+let onGhostCollision = () => {
+  lives--;
+  restartPacmanAndGhosts();
+  if (lives == 0) {
+  }
+};
+let createGhosts = () => {
+  ghosts = [];
+  for (let i = 0; i < ghostCount * 2; i++) {
+    let newGhost = new Ghost(
+      9 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      10 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      oneBlockSize,
+      oneBlockSize,
+      pacman.speed / 2,
+      ghostImageLocations[i % 4].x,
+      ghostImageLocations[i % 4].y,
+      124,
+      116,
+      6 + i
+    );
+    ghosts.push(newGhost);
+  }
+};
 // pacmans
 let pacman = new Pacman(
   oneBlockSize,
@@ -53,6 +95,30 @@ let pacmanSecond = new Pacman(
 let score = 0;
 let lives = 3;
 
+const keysPacman = {
+  ArrowLeft: DIRECTION_LEFT,
+  ArrowUp: DIRECTION_UP,
+  ArrowRight: DIRECTION_RIGHT,
+  ArrowDown: DIRECTION_BOTTOM,
+};
+
+const keysPacmanSecond = {
+  a: DIRECTION_LEFT,
+  w: DIRECTION_UP,
+  d: DIRECTION_RIGHT,
+  s: DIRECTION_BOTTOM,
+};
+const keyboardObservable = (pacman, keys) => {
+  const keyboardEvent$ = rxjs.fromEvent(window, "keydown").pipe(
+    rxjs.filter((event) => {
+      return Object.keys(keys).includes(event.key);
+    })
+  );
+
+  keyboardEvent$.subscribe((event) => {
+    pacman.nextDirection = keys[event.key];
+  });
+};
 const createNewPacman = () => {
   pacman = new Pacman(
     oneBlockSize,
@@ -70,6 +136,9 @@ const createNewPacman = () => {
     oneBlockSize / 5,
     pacmanSecondFrames
   );
+
+  keyboardObservable(pacman, keysPacman);
+  keyboardObservable(pacmanSecond, keysPacmanSecond);
 };
 
 // ciclo principal del juego
@@ -90,16 +159,24 @@ const gameInterval$ = rxjs.interval(1000 / fps).pipe(
   })
 );
 
-/* const restartPacmanAndGhosts = () => {
+const restartPacmanAndGhosts = () => {
   createNewPacman();
+  createGhosts();
 };
- */
 
 // little refactor
 // add ti ganme interval
+const runCollition = (aPacman) => {
+  if (aPacman.checkGhostCollision(ghosts)) {
+    onGhostCollision();
+  }
+};
 const update = () => {
   pacman.moveProcess();
   pacmanSecond.moveProcess();
+  updateGhosts();
+  runCollition(pacman);
+  runCollition(pacmanSecond);
 };
 
 // important needs changes
@@ -112,7 +189,7 @@ const drawFoods = () => {
           i * oneBlockSize + oneBlockSize / 3,
           oneBlockSize / 3,
           oneBlockSize / 3,
-          '#FEB897'
+          "#FEB897"
         );
       }
     }
@@ -122,48 +199,21 @@ const drawFoods = () => {
 // important need changes
 const draw = () => {
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  createRect(0, 0, canvas.width, canvas.height, 'black');
+  createRect(0, 0, canvas.width, canvas.height, "black");
   drawWalls();
   drawFoods();
   pacman.draw();
   pacmanSecond.draw();
+  drawGhosts();
   drawScore();
   drawRemainingLives();
 };
 
-createNewPacman();
+restartPacmanAndGhosts();
 
-const clickCanvas$ = rxjs.fromEvent(canvas, 'click');
+const clickCanvas$ = rxjs.fromEvent(canvas, "click");
 clickCanvas$
   .pipe(rxjs.take(1))
   .subscribe(() => gameInterval$.subscribe(), { once: true });
 
-const keyboardObservable = (pacman, keys) => {
-  const keyboardEvent$ = rxjs.fromEvent(window, 'keydown').pipe(
-    rxjs.filter((event) => {
-      return Object.keys(keys).includes(event.key);
-    })
-  );
-
-  keyboardEvent$.subscribe((event) => {
-    pacman.nextDirection = keys[event.key];
-  });
-};
-
-const keysPacman = {
-  ArrowLeft: DIRECTION_LEFT,
-  ArrowUp: DIRECTION_UP,
-  ArrowRight: DIRECTION_RIGHT,
-  ArrowDown: DIRECTION_BOTTOM,
-};
-
-const keysPacmanSecond = {
-  a: DIRECTION_LEFT,
-  w: DIRECTION_UP,
-  d: DIRECTION_RIGHT,
-  s: DIRECTION_BOTTOM,
-};
-
 // keyboard observer
-keyboardObservable(pacman, keysPacman);
-keyboardObservable(pacmanSecond, keysPacmanSecond);
